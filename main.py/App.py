c0eb536a417142c37edaccb2be6f3a6280eb216b
your_username <your_email@example.com>
2024-05-30 13:54:49 +1200

    i am adding a navigation bar

--- main.py/App.py
+++ main.py/App.py
@@ -2,20 +2,24 @@ from tkinter import *
 #from app_settings import *
 from os import *
 
-print("fun")
-
 w_width = 414
 h_height = 816
 
 bg_color = "#FFA500"
 
+
+
 class App():
+
+    
 
     def __init__(self):
 
         self.window = Tk()
         self.window.geometry(str(w_width) + "x" + str(h_height))
         self.window.title("Dish Lister")
+
+        #self.window.iconbitmap('c:/gui/codemy.ico')
 
         self.top_frame = Frame(background='orange', width=w_width, height= 100)
         self.top_frame.pack(side='top')
@@ -26,10 +30,10 @@ class App():
         self.bottom_frame = Frame(background='yellow', width=w_width, height=100)
         self.bottom_frame.pack(side='bottom')
 
-        self.home_button = Button(self.top_frame, text="Menu", height=5, width=5, bg='white')
+        self.home_button = Button(self.top_frame, text="Menu", height=5, width=5, bg='white', command=self.home_open)
         self.home_button.place(x=15,y=7)
 
-        self.exit_button = Button(self.bottom_frame, text="Exit", height=5, width=5, bg='white', command=exit)
+        self.exit_button = Button(self.bottom_frame, text="Exit", height=5, width=5, bg='white', command=self.exit)
         self.exit_button.place(x=15,y=7)
 
         self.dirname = path.dirname(__file__)
@@ -37,8 +41,42 @@ class App():
 
         print("the path is", self.filename)
 
+        my_menu = Menu()
+        self.config(menu=my_menu)
+
+        def our_command():
+            self.my_label = Label(text="womp womp").pack()
+
+            file_menu = Menu(my_menu)
+            my_menu.add_cascade(Label="File", menu=file_menu)
+            file_menu.add_command(Label="New...", command=our_command)
+            file_menu.add_separator()
+            file_menu.add_command(Label="Exit", command=quit)
+
+            edit_menu = Menu(my_menu)
+            my_menu.add_cascade(Label="Edit", menu=edit_menu)
+            edit_menu.add_command(Label="Cut", command=our_command)
+            edit_menu.add_command(Label="Copy", command=our_command)
+        
         self.window.mainloop()
 
 
+
+  
+
     def exit(self):
         self.window.destroy()
+
+    
+    def home_open(self):
+        print("yes")
+
+
+    
+
+
+
+    
+        
+      
+    
